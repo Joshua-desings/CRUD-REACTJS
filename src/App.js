4c317fc019b4114beffb6b803d9380590514fdf7
Joshua <64817207+Joshua-desings@users.noreply.github.com>
2023-11-29 16:12:20 -0400

    Mejor estructura del código, comentarios y css

--- src/App.js
+++ src/App.js
@@ -1,67 +1,65 @@
-import React, {useState, useEffect} from "react";
-import Formulario from "./Componentes/Formulario";
-import Tabla from "./Componentes/Tabla";
-import './App.css';
-
-
-
+import React, { useState, useEffect } from "react";
+import Formulario from "./Components/Formulario/Formulario";
+import Tabla from "./Components/Tabla/Tabla";
+import "./App.css";
 
 const Crud = () => {
-    const [editarData, setEditarData] = useState(null);
-    const [listado, setListado] = useState(() => {
+  // Estado para manejar la información de edición
+  const [editarData, setEditarData] = useState(null);
+  // Estado para manejar la lista de estudiantes
+  const [listado, setListado] = useState(() => {
+    // Obtener la información del local storage al iniciar la aplicación
+    const guardardatos = window.localStorage.getItem("listadoData");
+    return guardardatos ? JSON.parse(guardardatos) : [];
+  });
 
-        //Solicitar la información al localstorage
-        const guardardatos = window.localStorage.getItem("listadoData");
-        if (guardardatos) {
-            return JSON.parse(guardardatos);
-        }
-        else {
-            return []
-        }
-    });
+  // Efecto para guardar cambios en el local storage cuando la lista cambia
+  useEffect(() => {
+    window.localStorage.setItem("listadoData", JSON.stringify(listado));
+  }, [listado]);
 
-    
-    //Enviar la informacion en formato JSON al localstorage
-    useEffect(() => {
-        window.localStorage.setItem("listadoData", JSON.stringify(listado))
-    }, [listado])
+  // Función para agregar un estudiante a la lista
+  const agregarEstudiante = (estudiante) => {
+    setListado([...listado, estudiante]);
+  };
 
+  // Función para editar los datos de un estudiante en la lista
+  const editarEstudiante = (estudiante) => {
+    const edicion = listado.map((el) =>
+      el.id === estudiante.id ? estudiante : el
+    );
+    setListado(edicion);
+    setEditarData(null);
+  };
 
-    //Agregar un estudiante
-    const agregarEstudiante = (estudiante) => {
-        setListado([
-            ...listado,
-            estudiante
-        ])
-    };
-
-
-    //Editar datos de un estudiante
-    const editarEstudiante = (estudiante) => {
-        const edicion = listado.map(el => el.id === estudiante.id ? estudiante: el)
-        setListado(edicion)
-        setEditarData(null)
+  // Función para eliminar un estudiante de la lista
+  const eliminarEstudiante = (id) => {
+    const borrar = window.confirm(
+      `¿Desea borrar a este estudiante de la lista?`
+    );
+    if (borrar) {
+      const edicion = listado.filter((el) => el.id !== id);
+      setListado(edicion);
     }
+  };
 
-
-    //Eliminar un estudiante
-    const eliminarEstudiante = id => {
-        const borrar = window.confirm(`Desea borrar a este estudiante de la lista?`)
-
-        if (borrar) {
-            const edicion = listado.filter(el => el.id !== id)
-            setListado(edicion);
-        }
-    }
-
-
-    return <>
-    <h2>crud</h2>
-
-    <Formulario agregarEstudiante={agregarEstudiante} editarEstudiante={editarEstudiante} editarData={editarData}/>
-
-    <Tabla listado={listado} setEditarData={setEditarData} eliminarEstudiante={eliminarEstudiante}/>
+  return (
+    <>
+      <h2>Formulario para Estudiantes</h2>
+      {/* Componente de formulario para agregar y editar estudiantes */}
+      <Formulario
+        agregarEstudiante={agregarEstudiante}
+        editarEstudiante={editarEstudiante}
+        editarData={editarData}
+      />
+      {/* Componente de tabla para mostrar la lista de estudiantes */}
+      <Tabla
+        listado={listado}
+        setEditarData={setEditarData}
+        eliminarEstudiante={eliminarEstudiante}
+      />
     </>
-}
+  );
+};
 
 export default Crud;
